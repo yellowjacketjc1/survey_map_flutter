1ddb279775bdd69a35d1b20ab1673ce5371f068e
yellowjacketjc1 <yellowjacketjc1@users.noreply.github.com>
2025-10-17 18:30:32 +0000

    deploy: 7d98daf8209c2f13363f8b4f207eaad6888d8ecb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3080059694"
+    serviceWorkerVersion: "1519203577"
   }
 });
